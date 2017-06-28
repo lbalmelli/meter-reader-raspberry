147b0a4e30115f71d98d11b552502ef0ed385575
Laurent Balmelli <laurent.balmelli@gmail.comgit config --global user.name Laurent>
2017-06-28 01:21:25 -0500

    Initial commit

---
+++ TestCamera.py
@@ -0,0 +1,30 @@
+#coding: utf-8
+from picamera import PiCamera
+from time import sleep
+import sys
+
+import pytesseract
+#from PIL import Image
+
+
+#pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+#tessdata_dir_config = '--tessdata-dir "/usr/bin/"'
+# use this quote: ' 
+#
+
+#print sys.getdefaultencoding()
+
+#camera = PiCamera()
+
+#camera.rotation = 180
+#camera.start_preview()
+#sleep(5)
+#camera.capture('/home/pi/Laurent/Dev/PiCamera/pics/img1.jpg')
+#filenames = []
+#filenames.append('/home/pi/Laurent/Dev/PiCamera/pics/img1.jpg')
+#img = Image.open('/home/pi/Laurent/Dev/PiCamera/pytesseract/src/test.png')
+#img.load()
+#print pytesseract.image_to_string(img,config='outputbase digits')
+#print pytesseract.image_to_string(img)
+#camera.stop_preview()
+
